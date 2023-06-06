823f635ba186afa192f31bbb0592e9756aeda7e9
Aman Singh <amansinghs7010@gmail.com>
2023-06-06 18:10:18 -0400

    add ngicons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NgIconsModule } from '@ng-icons/core';
+import {
+  heroPlusCircle,
+  heroArrowLeftOnRectangle,
+} from '@ng-icons/heroicons/outline';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -14,7 +19,11 @@ import { CardsComponent } from './cards/cards.component';
     DashboardComponent,
     CardsComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgIconsModule.withIcons({ heroPlusCircle, heroArrowLeftOnRectangle }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
