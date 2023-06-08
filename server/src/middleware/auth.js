41dbfbdd04b9c261197f418558923e5017dd1e2f
Aman Singh <amansinghs7010@gmail.com>
2023-06-07 20:20:47 -0400

    added middleware for authenticated routes

---
+++ server/src/middleware/auth.js
@@ -0,0 +1,21 @@
+const OAuthToken = require("../models/oAuthToken");
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const oAuthToken = await OAuthToken.findOne({ token: token });
+
+    if (!oAuthToken?.accessToken) {
+      throw new Error();
+    }
+
+    req.accessToken = oAuthToken.accessToken;
+
+    next();
+  } catch (e) {
+    console.log("[middleware/auth] Token could not be verified");
+    res.status(401).send();
+  }
+};
+
+module.exports = auth;
--- server/src/routers/cards.js
+++ server/src/routers/cards.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { getCards, saveCard } = require("../services/cards");
+const auth = require("../middleware/auth");
 
 const router = new express.Router();
 
-router.get("/api/cards", getCards);
+router.get("/api/cards", auth, getCards);
 
-router.post("/api/cards", saveCard);
+router.post("/api/cards", auth, saveCard);
 
 module.exports = router;
