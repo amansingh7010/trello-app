8911bd06b86cba8a958b788bd02029f86f5d32cf
Aman Singh <amansinghs7010@gmail.com>
2023-06-09 09:13:02 -0400

    move index.js back

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,6 @@
-const app = require("../src/app");
+const app = require("./app");
 const port = process.env.PORT || 9000;
 
 app.listen(port, () => {
   console.log("[index] Server is up on port " + port);
 });
-
-module.exports = app;
